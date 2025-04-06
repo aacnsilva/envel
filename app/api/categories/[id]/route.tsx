85f050affd7bc800e6a70050745b26455a261e2e
António Silva <aacnsilva@hotmail.com>
2025-04-06 21:21:29 +0100

    fix build

--- app/api/categories/[id]/route.tsx
+++ app/api/categories/[id]/route.tsx
@@ -1,18 +1,21 @@
 import { NextResponse } from "next/server";
 import { deleteCategory, getCategoryById, updateCategory } from "@/services/category";
 
-export async function GET(request: Request, { params }: { params: { id: string } }) {
-  const category = await getCategoryById(params.id);
+export async function GET(request: Request, { params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params;
+  const category = await getCategoryById(id);
   return NextResponse.json(category);
 }
 
-export async function PUT(request: Request, { params }: { params: { id: string } }) {
+export async function PUT(request: Request, { params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params;
   const category = await request.json();
-  const updatedCategory = await updateCategory(params.id, category);
+  const updatedCategory = await updateCategory(id, category);
   return NextResponse.json(updatedCategory);
 }
 
-export async function DELETE(request: Request, { params }: { params: { id: string } }) {
-  const deletedCategory = await deleteCategory(params.id);
+export async function DELETE(request: Request, { params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params;
+  const deletedCategory = await deleteCategory(id);
   return NextResponse.json(deletedCategory);
 }
